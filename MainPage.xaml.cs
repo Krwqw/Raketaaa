f94a6363956fe78ab64afdfdcd1c9aeb482f55d9
Nadi <Nadi@NADEZHDIN-PK1>
2025-05-30 13:32:50 +0300

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace raketa1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnFire(object sender, EventArgs e)
+        {
+            Fire1.IsVisible = true;
+            Fire2.IsVisible = true;
+            Fire3.IsVisible = true;
+            Window1.Fill = Colors.Aqua;
+            Window2.Fill = Colors.Aqua;
+        }
+
+
+        private async void OffFire(object sender, EventArgs e)
+        {
+
+            Fire1.IsVisible = false;
+            Fire2.IsVisible = false;
+            Fire3.IsVisible = false;
+            Window1.Fill = Colors.Black;
+            Window2.Fill = Colors.Black;
+        }
+    }
+}
+
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace raketa1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
